00c41ddafccb4becccdd81b632e27d2033aadc17
Noodle Dash OL <noodle.dash.ol@gmail.com>
2024-07-09 19:34:06 +0700

    GEO-38 Forbidden number label

--- src/commands/ChangeLabelCommand.ts
+++ src/commands/ChangeLabelCommand.ts
@@ -22,11 +22,19 @@ export default class ChangeLabelCommand extends BaseCommand {
 
     public execute(p: (Elem | string | number)[]) {
         if (p.length == 2 && typeof p[0] == "string" && typeof p[1] == "string") {
+            if (!isNaN(parseFloat(p[1]))) {
+                return
+            }
+
             const elem = Elems.instance.getByLabel(p[0])
             if (elem != null && elem instanceof LabelElem) {
                 Elems.instance.changeLabel(elem, p[1])
             }
         } else if (p.length == 2 && p[0] instanceof LabelElem && typeof p[1] == "string") {
+            if (!isNaN(parseFloat(p[1]))) {
+                return
+            }
+
             Elems.instance.changeLabel(p[0], p[1])
         } 
     }
