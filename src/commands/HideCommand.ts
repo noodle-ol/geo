0b932a432adf5a9b66a0b25b78ca918ebcc382ef
Noodle Dash OL <noodle.dash.ol@gmail.com>
2024-07-09 22:06:02 +0700

    GEO-40 Hide/show by click element

--- src/commands/HideCommand.ts
+++ src/commands/HideCommand.ts
@@ -1,5 +1,6 @@
 import BaseElem from "../elements/BaseElem"
 import Elem from "../elements/IElem"
+import PointElems from "../elements/PointElems"
 import BaseCommand from "./BaseCommand"
 import Commands from "./Commands"
 
@@ -19,9 +20,23 @@ export default class HideCommand extends BaseCommand {
         return HideCommand.instance
     }
 
+    private hideElemNoParam() {
+        Commands.instance.setCurrentCommand(HideCommand.instance)
+    }
+
     public execute(p: (Elem | string | number)[]) {
         if (p.length == 1 && p[0] instanceof BaseElem) {
             p[0].hide()
+        } else {
+            this.hideElemNoParam()
+        }
+    }
+
+    public onmousedown(e: MouseEvent) {
+        const [clientX, clientY] = [e.clientX - globalThis.mainLeftMargin, e.clientY]
+        let pointElem = PointElems.instance.find(clientX, clientY)
+        if (pointElem != null && pointElem.getIsShow()) {
+            pointElem.hide()
         }
     }
 }
